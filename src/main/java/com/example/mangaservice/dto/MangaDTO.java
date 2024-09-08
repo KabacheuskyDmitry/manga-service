d9e2f4d133b583b44e5bfd83fc4fbc660cc50cc4
KabacheuskyDmitry <dimmmon4@gmail.com>
2024-09-08 21:52:38 +0300

    update

--- src/main/java/com/example/mangaservice/dto/MangaDTO.java
+++ src/main/java/com/example/mangaservice/dto/MangaDTO.java
@@ -7,33 +7,40 @@ public class MangaDTO {
     private String author;
     private Double rating;
 
-    // Constructors, Getters, and Setters
     public Long getId() {
+
         return id;
     }
 
     public void setId(Long id) {
+
         this.id = id;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
-    public String getAuthor() { return author; }
+    public String getAuthor() {
+        return author; }
 
     public void setAuthor(String author) {
+
         this.author = author;
     }
     public Double getRating() {
+
         return rating;
     }
 
     public void setRating(Double rating) {
+
         this.rating = rating;
     }
 
